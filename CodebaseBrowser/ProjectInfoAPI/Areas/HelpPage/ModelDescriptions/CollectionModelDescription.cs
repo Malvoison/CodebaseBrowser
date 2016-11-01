69938f7b96ceb434fad6749be26358c0983ee992
kwatts <allawyer66@gmail.com>
2016-11-01 08:44:40 -0500

    Initial checkin

---
+++ CodebaseBrowser/ProjectInfoAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ProjectInfoAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ CodebaseBrowser/ProjectInfoAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProjectInfoAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
