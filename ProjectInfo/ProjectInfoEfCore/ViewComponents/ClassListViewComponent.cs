721023b46b161f3f88862c9e2a204117530ddce0
kwatts <allawyer66@gmail.com>
2017-01-11 13:36:16 -0600

    Added ClassListViewComponent

---
+++ ProjectInfo/ProjectInfoEfCore/ViewComponents/ClassListViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectInfoEfCore.Models;
+
+namespace ProjectInfoEfCore.ViewComponents
+{
+    public class ClassListViewComponent : ViewComponent
+    {
+        private readonly ProjectInfoContext _context;
+
+        public ClassListViewComponent(ProjectInfoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(Guid projectsId)
+        {
+            var items = await GetItemsAsync(projectsId);
+            return View(items);
+        }
+
+        private Task<List<Classes>> GetItemsAsync(Guid projectsId)
+        {
+            var classList = from t1 in _context.Classes
+                            let t2s = from t2 in _context.ClassMap
+                                      where t2.ProjectsId == projectsId
+                                      select t2.ClassesId
+                            where t2s.Contains(t1.ClassesId)
+                            select t1;
+
+            return classList.ToListAsync();
+        }
+    }
+}
