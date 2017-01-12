45af31c0d69c3e51f1702b231b4092bbba1ba4dc
kwatts <allawyer66@gmail.com>
2017-01-12 15:29:43 -0600

    End of day check-in

---
+++ ProjectInfo/ProjectInfoEfCore/ViewComponents/ClassUsedByViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectInfoEfCore.Models;
+
+
+namespace ProjectInfoEfCore.ViewComponents
+{
+    public class ClassUsedByViewComponent : ViewComponent
+    {
+        private readonly ProjectInfoContext _context;
+
+        public ClassUsedByViewComponent(ProjectInfoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(Guid? classesId)
+        {
+            var items = await GetItemsAsync(classesId);
+            return View(items);
+        }
+
+        private Task<List<Projects>> GetItemsAsync(Guid? classesId)
+        {
+            var projMap = from t2 in _context.ClassMap
+                          where t2.ClassesId == classesId
+                          select t2.ProjectsId;
+
+            var projList = from t1 in _context.Projects
+                           where projMap.Contains(t1.ProjectsId)
+                           select t1;
+
+            return projList.ToListAsync();
+        }
+    }
+}
